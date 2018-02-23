4b16626778f57fdaed5fff879050745b7d57e602
kamlesh kumar <kamlesh@kumar>
2018-02-23 23:27:33 +0530

    change in Master

--- XML_Value_Change/src/com/xml/Animal.java
+++ XML_Value_Change/src/com/xml/Animal.java
@@ -4,8 +4,8 @@ public class Animal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Animal");
-		System.out.println("Merge");
+		System.out.println("Master");
+		
 	}
 
 }
