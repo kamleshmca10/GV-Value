094ad9639e234238993713fd01798eea669e4b11
kamlesh kumar <kamlesh@kumar>
2018-02-23 23:38:19 +0530

    change class

---
+++ XML_Value_Change/src/com/xml/Master.java
@@ -0,0 +1,11 @@
+package com.xml;
+
+public class Master {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Master");
+		System.out.println("New Line-M");
+	}
+
+}
